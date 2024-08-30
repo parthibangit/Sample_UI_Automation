3ec0bc0a36d4b7b496be1332cb2c27b191543867
parthibangit <sarathipartha04@gmail.com>
2024-08-30 15:51:45 +0530

    Added util method to use in test methods dynamically.

--- src/main/java/org/example/utilities/Utils.java
+++ src/main/java/org/example/utilities/Utils.java
@@ -1,4 +1,34 @@
 package org.example.utilities;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class Utils {
+
+    private Utils() {
+
+    }
+
+    public static String getRandomEmail(int length) {
+
+        String randomValue = RandomStringUtils.randomAlphabetic(length);
+        return randomValue+"@gmail.com";
+    }
+
+    public static String getRandomPassword(int length) {
+
+        return RandomStringUtils.randomAlphabetic(length);
+    }
+
+    public static String getFirstName(int length) {
+
+        String name = RandomStringUtils.randomAlphabetic(length);
+        return name+" first";
+    }
+
+    public static String getLastName(int length) {
+
+        String name = RandomStringUtils.randomAlphabetic(length);
+        return name+" last";
+    }
+
 }

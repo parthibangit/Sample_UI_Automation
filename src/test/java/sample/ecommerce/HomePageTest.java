f0b7b9777c70404a6ec15e6e4c13423a10d0ef62
parthibangit <sarathipartha04@gmail.com>
2024-08-31 13:16:42 +0530

    Added new test methods.

--- src/test/java/sample/ecommerce/HomePageTest.java
+++ src/test/java/sample/ecommerce/HomePageTest.java
@@ -1,20 +1,18 @@
 package sample.ecommerce;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.example.base.BaseTest;
-import org.example.configuration.PropertyReader;
-import org.example.pageobjects.SignInPage;
+import org.example.base.BaseHelper;
+import org.example.pages.ContactUsPage;
+import org.example.pages.HomePage;
+import org.example.pages.MyContactPage;
+import org.example.pages.SignInPage;
 import org.example.utilities.Utils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class HomePageTest extends BaseTest {
-
-    private static final Logger logger = LogManager.getLogger(HomePageTest.class);
+public class HomePageTest extends BaseHelper {
 
     @Test(priority = 1)
-    public void create_new_user() {
+    public void create_new_user_and_verify_success_message() {
 
         SignInPage signInPage = new SignInPage(driver);
         signInPage.navigateToSignInPage();
@@ -42,47 +40,113 @@ public class HomePageTest extends BaseTest {
     @Test(priority = 3)
     public void verify_shopping_cart_section_is_empty_for_new_user() {
 
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.createNewUser();
+
+        MyContactPage myContactPage = new MyContactPage(driver);
+        myContactPage.clickShopCart();
+        boolean status = myContactPage.verifyInfoMessageDisplayed();
+        Assert.assertTrue(status);
+        signInPage.signOut();
     }
 
     @Test(priority = 4)
     public void verify_my_order_section_is_empty_for_new_user() {
 
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.createNewUser();
+
+        MyContactPage myContactPage = new MyContactPage(driver);
+        myContactPage.clickMyAccountOrderHistory();
+        boolean status = myContactPage.verifyInfoMessageDisplayed();
+        Assert.assertTrue(status);
+        signInPage.signOut();
     }
 
     @Test(priority = 5)
     public void verify_my_credit_slip_section_is_empty_for_new_user() {
 
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.createNewUser();
+
+        MyContactPage myContactPage = new MyContactPage(driver);
+        myContactPage.clickMyAccountCreditSlips();
+        boolean status = myContactPage.verifyInfoMessageDisplayed();
+        Assert.assertTrue(status);
+        signInPage.signOut();
     }
 
     @Test(priority = 6)
-    public void search_product_verify_search_result() {
+    public void search_product_and_verify_search_result() {
 
+        String productName = "shirts";
+        HomePage homePage = new HomePage(driver);
+        homePage.searchProduct(productName);
+        homePage.verifySearchResultsName(productName);
+        homePage.verifySearchResultsContainsProductName(productName);
     }
 
     @Test(priority = 7)
     public void send_query_to_customer_service_and_verify_success_message() {
 
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.createNewUser();
+
+        ContactUsPage contactUsPage = new ContactUsPage(driver);
+        contactUsPage.navigateToContactUsPage();
+        boolean headerStatus = contactUsPage.verifyContactUsHeaderIsDisplayed();
+        Assert.assertTrue(headerStatus);
+        contactUsPage.fillInformationToContact("Customer service", Utils.getMessage(7));
+        boolean successMessageStatus = contactUsPage.verifySuccessMessageDisplayed();
+        Assert.assertTrue(successMessageStatus);
     }
 
     @Test(priority = 8)
     public void verify_success_message_displays_when_user_subscribe_the_newsletter() {
 
+        SignInPage signInPage = new SignInPage(driver);
+        String email = signInPage.createNewUser();
+
+        HomePage homePage = new HomePage(driver);
+        homePage.scrollToFooterSection();
+        homePage.subscribeTheNewsLetter(email);
+        boolean status = homePage.verifyNewsLetterSuccessMessageIsDisplayed();
+        Assert.assertTrue(status);
     }
 
     @Test(priority = 9)
     public void verify_success_message_displays_when_user_retrieve_password_using_forgot_password() {
 
+        SignInPage signInPage = new SignInPage(driver);
+        String email = signInPage.createNewUser();
+        signInPage.signOut();
+
+        signInPage.navigateToSignInPage();
+        signInPage.retrievePassword(email);
+        boolean status = signInPage.verifyRetrievePasswordEmailConfirmationIsDisplayed();
+        Assert.assertTrue(status);
     }
 
     @Test(priority = 10)
     public void verify_company_address_information_available_on_footer() {
 
+        HomePage homePage = new HomePage(driver);
+        homePage.scrollToFooterSection();
+        boolean status = homePage.verifyAddressIsPresent();
+        Assert.assertTrue(status);
     }
 
     @Test(priority = 11)
     public void verify_info_message_displays_when_customer_not_having_address() {
 
-    }
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.createNewUser();
 
+        MyContactPage myContactPage = new MyContactPage(driver);
+        myContactPage.clickMyAccountMyAddress();
+        boolean status = myContactPage.verifyInfoMessageDisplayed();
+        Assert.assertTrue(status);
+        signInPage.signOut();
+    }
 
 }
